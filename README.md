0e3fb69e6526e82212108c97b07eba6372493799
Elliot Glasenk <eglasenk@box.com>
2019-06-14 15:53:05 -0400

    Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+README.md
+# square tech assessment
+## create an event monitoring system that keeps track of events, e.g. (Wildfire, 10),where wildfire is name and 10 is priorty.
+#desired functionality
+##add event
+##remove event
+##keep track of the top priority event
--- eventSystem.js
+++ eventSystem.js
@@ -1,11 +1,4 @@
-//square tech assessment
-//create an event monitoring system
-//that keeps track of events, e.g. (Wildfire, 10)
-//where wildfire is name and 10 is priorty
-//desired functionality
-//add event
-//remove event
-//keep track of the top priority event
+
 
 let event = function(id, name, priority) {
   this.id = id;
